a15b22ac1b3c0785a9d73bfebcf3ecce14669468
naomiHauret <hauretnaomi@gmail.com>
2023-06-18 11:59:06 +0200

    feat: update sign in screen ui

--- packages/app/features/sign-in/screen.tsx
+++ packages/app/features/sign-in/screen.tsx
@@ -1,18 +1,30 @@
 import { H1, H2, Paragraph, YStack } from '@my/ui'
 import SignInForm from './Form'
+import { useRouter } from 'solito/router'
+import { useEffect } from 'react'
+import { useCurrentUser } from 'app/hooks'
 
 /**
  * Universal screen. Allows the user to sign in to the app via Magic SDK.
  * @returns Sign in page
  */
 export function SignInScreen() {
-  return (
-    <YStack flexGrow={1} jc="center" ai="center" p="$4" space>
-      <YStack space="$4">
-        <YStack space="$1">
-          <H2 ta="center">🏕️</H2>
+  const { push } = useRouter()
+  const {  userInfo } = useCurrentUser()
 
-          <H1 ta="center">Your cozy bookish banters awaits.</H1>
+  useEffect(() => {
+    if(userInfo?.publicAddress) {
+      push('/')
+    }
+  }, [userInfo?.publicAddress])
+
+  return (
+    <YStack flexGrow={1} jc="center" ai="center"  px="$4" space>
+      <YStack space="$2">
+        <YStack space="$1">
+          <H2 pt="$8" ta="center">🏕️</H2>
+
+          <H1 size="$6" fontFamily="$body" fontWeight="bold" ta="center">Your cozy bookish banters awaits.</H1>
         </YStack>
         <Paragraph ta="center">
           Sign-in to join book clubs and connect with fellow readers on Hearthside Hangouts !
